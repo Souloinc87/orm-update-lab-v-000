1fae99867cbd23d0bba4001f150973faf2fe70e3
Marcus  = Soulo <souloinc87@gmail.com>
2018-11-09 05:00:26 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -42,8 +42,10 @@ attr_reader :id
     end 
   end
 
-  # Remember, you can access your database connection anywhere in this class
-  #  with DB[:conn]
-
+  def self.create(name, grade)
+    student = Student.new(name, grade)
+    student.save
+    student 
+  end 
 
 end
