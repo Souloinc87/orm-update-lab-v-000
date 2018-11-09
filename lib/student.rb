689749afbb55c0609ee9d0c454fd147be2f6b0ef
Marcus  = Soulo <souloinc87@gmail.com>
2018-11-09 05:08:10 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -49,11 +49,10 @@ attr_reader :id
   end
 
   def self.new_from_db(row)
-    student = self.new
-    student.id = row[0]
-    student.name = row[1]
-    student.grade = row[2]
-    student 
+    id = row[0]
+    name = row[1]
+    grade = row[2]
+    self.new(id, name, grade)
   end
 
 end
