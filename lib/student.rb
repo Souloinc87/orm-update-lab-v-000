964e64da63abeea73ee44e90af3410948ed5b4d4
Marcus  = Soulo <souloinc87@gmail.com>
2018-11-09 05:16:53 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -63,7 +63,7 @@ attr_reader :id
 
   def update
     sql = "UPDATE students SET name = ?, grade = ? WHERE id = ?"
-    DB[:conn].execute(sql,, self.name, self.grade, self.id)
+    DB[:conn].execute(sql, self.name, self.grade, self.id)
   end
 
 end
